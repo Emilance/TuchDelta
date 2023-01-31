631521fa00aba606d51099a35fb05ac9de7622d4
Estherfisayomi12 <fisayomi2602@gmail.com>
2023-01-31 13:15:44 +0100

    designed the admin login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Login from './Pages/Login'
-import Homepage from './Pages/HomePage'
-import { Route, Routes } from 'react-router-dom'
-import AboutPage from './Pages/AboutPage'
-import ContactPage from './Pages/ContactPage'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Login from "./Pages/Login";
+import Homepage from "./Pages/HomePage";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "./Pages/AboutPage";
+import ContactPage from "./Pages/ContactPage";
+import AdminLogin from "./Pages/AdminLogin";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="app ">
-        <Routes>
-              <Route path='/' element={<Homepage/> } />
-              <Route path='/login' element={<Login/>} />
-              <Route path='/about' element={<AboutPage/>} />
-              <Route path='/contact' element={<ContactPage/>} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/admin" element={<AdminLogin />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
